b1abade2777aed6b1144497d0730aef87967fdac
Legacynnn <danmelomour@gmail.com>
2025-04-02 02:40:41 -0300

    first commit

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
+import { admin } from "better-auth/plugins"
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -10,6 +11,11 @@ export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   trustedOrigins: [
     "http://localhost:3000",
+  ],
+  plugins: [
+    admin({
+      adminUserIds: ["SWp4kVpPGfUvuXbGoso3hpPXyIWxg6EI"]
+    }) 
   ],
   emailAndPassword: {
     enabled: true,
